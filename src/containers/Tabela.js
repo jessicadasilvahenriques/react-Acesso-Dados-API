598b8a9aa5c0e4f018a67ea7074dd82c76f5bd6e
Andre Ramos <aluno20710@ipt.pt>
2021-07-01 14:53:05 +0100

    Lista de receitas

--- src/containers/Tabela.js
+++ src/containers/Tabela.js
@@ -11,7 +11,7 @@ function CabecalhoTabela() {
                 <th>Nome</th>
                 <th>Ingredientes</th>
                 <th>Modo de Preparo</th>
-               
+
             </tr>
         </thead>
     )
@@ -22,42 +22,86 @@ function CabecalhoTabela() {
 const CorpoTabela = (props) => {
     // esta função 'interna' irá ler e processar todos
     // os objetos definidos dentro do array 'dadosRecebidos'
-    const rows = props.dadosDasReceitas.map((row, index) => {
-        return (
-            <tr key={row.iDreceita}>
-                <td>{row.descricao}</td>
-                <td>{row.ingredientes}</td>
-                <td>{row.modoPreparo}</td>
-                <td>
-                    <button className="btn btn-outline-danger"
-                            //onClick={()=>props.receitasAremover(index)}
-                    >
-                        Apagar Receita
-                    </button>
-                </td>
-            </tr>
-        )
-    })
+    var rows
+    if (props.dadosDasReceitas) {
+        rows = props.dadosDasReceitas.map((row, index) => {
+            return (
+                <tr key={row.iDreceita}>
+                    <td>{row.descricao}</td>
+                    <td>{row.ingredientes}</td>
+                    <td>{row.modoPreparo}</td>
+                    <td>
+                        <button className="btn btn-outline-danger"
+                            onClick={() => props.receitasAremover(index)}
+                        >
+                            Apagar Receita
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+    else {
+        rows = () => { return (<tr><td></td></tr>) }
+    }
 
     // valor devolvido pela função 'CorpoTabela'
     return (<tbody>{rows}</tbody>)
 }
 
+async function getReceitas() {
+    //ler os dados da API
+    let resposta = await fetch('api/ReceitasAPI');
+    if (!resposta.ok) {
+        //não foi recido o código 200 do HTTP
+        console.error("Não conseguimos ler os dados da API. Código: " + resposta.status);
+    }
+    return await resposta.json();
+}
+
 // componente que junta os dois sub-componentes, formando um novo 'componente'
 class Tabela extends React.Component {
-    
+
+    constructor(props) {
+        super(props); // esta é sempre a primeira instrução
+
+        this.state = {
+            /**
+            * array que irá conter os dados das fotos, vindas da API
+            */
+            receitas: []
+        }
+    }
+
+    componentDidMount() {
+        this.LoadReceitas();
+    }
+
+    async LoadReceitas() {
+        /**
+         * Tarefas:
+         * 1- Ler os dados da API
+         * 2- Atualizar os dados na var. state
+         */
+        try {
+            let receitasVindasAPI = await getReceitas();
+            this.setState({ receitas: receitasVindasAPI });
+        } catch (error) {
+            console.error("Erro na leitura das receitas da API", error)
+        }
+    }
+
     render() {
-        console.log(this.props);
+        const { receitas } = this.state;
         // estamos a ler os dados que são recebidos pelo componente
         // <=> this.props.dadosAlunos
-        const { dadosDasReceitas} = this.props
 
         return (
             <table className="table table-striped table-success">
                 <CabecalhoTabela />
                 {/* o parâmetro 'dadosRecebidos' irá ajudar ao processamento
                     dos dados que vêm da componente 'mãe' */}
-                <CorpoTabela dadosDasReceitas={dadosDasReceitas}/>
+                <CorpoTabela dadosDasReceitas={receitas} />
             </table>
         )
     }
