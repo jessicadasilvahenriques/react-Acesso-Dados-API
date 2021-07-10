0320c52ae4358c05e5cc3b117f72eb75dae4c849
jessicadasilvahenriques <62100786+jessicadasilvahenriques@users.noreply.github.com>
2021-07-10 19:05:54 +0100

    Adição de um Carousel de imagens para a página inicial + começo de CSS

--- src/pages/Adicionar.js
+++ src/pages/Adicionar.js
@@ -1,12 +1,20 @@
 
 import React from 'react'
 import Formulario from "../containers/Formulario";
+import Carousel from "../containers/Carousel";
+import './Adicionar.css'
+import '../App.css'
 
 const Adicionar = () => {
         return (
           <div className="Adicionar">
+            <div className ="titulo-adicionar">
+             <h3>ADICIONAR RECEITA</h3>
+            </div>
              <Formulario />
           </div>
+
+          
         );
 };
       
